67f0ced0dfc67c7ba6e5adbe515df2a4743306ed
tangfengfordream <tangfengfordream@gmail.com>
2023-12-04 23:09:03 +0800

    使用HTML创建组件的基本的DOM结构

--- vue3/vue-project/src/pages/Wawaji.vue
+++ vue3/vue-project/src/pages/Wawaji.vue
@@ -1,3 +1,24 @@
 <template>
-    
+    <div class="page-portrait" id="page-portrait">
+        <div id="pageContainer" class="page-container game-box">
+            <div class="poster-main">
+                <ul class="poster-list">
+                    <li class="item lw1"><img src="/images/dx-lw1.png" alt=""></li>
+                    <li class="item lw2"><img src="/images/dx-lw2.png" alt=""></li>
+                    <li class="item lw3"><img src="/images/dx-lw3.png" alt=""></li>
+                    <li class="item lw4"><img src="/images/dx-lw4.png" alt=""></li>
+                    <li class="item lw5"><img src="/images/dx-lw5.png" alt=""></li>
+                    <li class="item lw6"><img src="/images/dx-lw6.png" alt=""></li>
+                </ul>
+            </div>
+            <div id="left" class="left-btn"></div>
+            <div id="stop" class="button"></div>
+            <div id="right" class="right-btn"></div>
+            <div class="zhua-top">
+                <span class="zhua-zuo"></span>
+                <span class="zhua-zhu"></span>
+                <div class="zhua zhuamove"></div>
+            </div>
+        </div>
+    </div>
 </template>
