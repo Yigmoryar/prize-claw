8d03288df9940c6486edc0fec72c5335f4bc4e33
tangfengfordream <tangfengfordream@gmail.com>
2023-12-05 00:24:35 +0800

    第二步
    1、给相应的HTML结点加上背景图片
    2、给相应的HTML定好位置
    3、给抓手加上动画
    4、使用tailwind写相应的css代码

--- vue3/vue-project/src/pages/Wawaji.vue
+++ vue3/vue-project/src/pages/Wawaji.vue
@@ -15,10 +15,99 @@
             <div id="stop" class="button"></div>
             <div id="right" class="right-btn"></div>
             <div class="zhua-top">
-                <span class="zhua-zuo"></span>
                 <span class="zhua-zhu"></span>
+                <span class="zhua-zuo"></span>
                 <div class="zhua zhuamove"></div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.page-portrait {
+    @apply h-full mx-auto;
+}
+
+.page-container {
+    @apply h-full w-[640px] bg-[url('/images/dx-bg2.jpg')] bg-no-repeat mx-auto relative;
+}
+
+.poster-main {
+    @apply absolute left-0 top-[560px] h-[120px] w-full max-w-full;
+
+    .poster-list {
+        @apply absolute left-0 top-0 h-full w-full;
+
+        .item {
+            position: absolute;
+            top: 0;
+            height: 120px;
+            widows: 125px;
+        }
+
+        .lw1 {
+            left: 128px;
+        }
+
+        .lw2 {
+            left: 253px;
+        }
+
+        .lw3 {
+            left: 378px;
+        }
+
+        .lw4 {
+            left: 128px;
+            top: 20px;
+
+        }
+
+        .lw5 {
+            left: 253px;
+            top: 20px;
+        }
+
+        .lw6 {
+            left: 378px;
+            top: 20px;
+        }
+    }
+}
+
+.zhua-top {
+    @apply absolute h-[490px] w-full top-[118px] left-0 mx-auto;
+
+    .zhua-zuo {
+        @apply absolute w-[58px] h-[17px] top-0 left-1/2 ml-[-29px] bg-[url('/images/dx-zuo.png')];
+
+    }
+
+    .zhua-zhu {
+        @apply absolute w-[22px] h-[140px] top-0 left-1/2 ml-[-11px] bg-[url('/images/dx-gan.png')]
+    }
+
+    .zhuamove {
+        @apply absolute w-[135px] h-[123px] top-[138px] left-1/2 ml-[-67.5px] bg-[url('/images/dx-zhua1.png')] bg-center bg-no-repeat;
+        animation: run 0.6s steps(1) infinite;
+    }
+}
+
+@keyframes run {
+  0% {
+    background-image: url(/images/dx-zhua3.png);
+  }
+  25% {
+    background-image: url(/images/dx-zhua2.png);
+  }
+  50% {
+    background-image: url(/images/dx-zhua1.png);
+  }
+  75% {
+    background-image: url(/images/dx-zhua2.png);
+  }
+  100% {
+    background-image: url(/images/dx-zhua3.png);
+  }
+}
+</style>
